39f35e409b68102736145ebaf0e67d857ae83d9c
fabiodelli <ntqdel@hotmail.com>
2023-04-26 16:35:44 +0200

    add header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name:'appHeader',
+    name: 'appHeader',
     data() {
 
 
@@ -11,10 +11,26 @@ export default {
 
 <template>
     <div class="header">
-        <div class="container_70 d-flex  ">
-            <div class="left">ciao</div>
-            <div class="center">ciao</div>
-            <div class="right">ciao</div>
+        <div class="container_70 d-flex">
+            <div class="left">
+                <img src="/img/logo-dark.png" alt="">
+            </div>
+            <div class="center">
+                <ul>
+                    <li>HOME</li>
+                    <li>LANDING</li>
+                    <li>PAGES</li>
+                    <li>DOCS</li>  
+                </ul>
+            </div>
+            <div class="right d-flex">
+                
+                <div><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></div>
+                <div class="toggle_nav"><font-awesome-icon icon="fa-brands fa-stack-overflow" /></div>
+                <div class="toggle_nav"><font-awesome-icon icon="fa-regular fa-user" /></div>
+                <div class="toggle_nav"><font-awesome-icon icon="fa-brands fa-github" /></div>
+                
+            </div>
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import '../node_modules/bootstrap'
 import './assets/_app.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {  faMagnifyingGlass, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faUserSecret,faStackOverflow,faUser,faGithub,faMagnifyingGlass)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
