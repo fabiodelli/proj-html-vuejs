55537a423b0dc1f33b9652784f24451f9d5f4bc0
fabiodelli <ntqdel@hotmail.com>
2023-04-27 02:24:56 +0200

    modify wave

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -19,9 +19,9 @@ export default {
                 <div class="center">
                     <ul>
                         <li>HOME</li>
-                        <li>LANDING</li>
-                        <li>PAGES</li>
-                        <li>DOCS</li>
+                        <li>LANDING <font-awesome-icon icon="fa-solid fa-chevron-down" size="2xs" /></li>
+                        <li>PAGES <font-awesome-icon icon="fa-solid fa-chevron-down" size="2xs" /></li>
+                        <li>DOCS <font-awesome-icon icon="fa-solid fa-chevron-down" size="2xs" /></li>
                     </ul>
                 </div>
                 <div class="right d-flex">
@@ -33,8 +33,8 @@ export default {
 
                 </div>
             </div>
-            <div class="header_bottom">
-                <div class="header_bl">
+            <div class="jumbo">
+                <div class="jumbo_left">
                     <div class="dev">Development</div>
                     <h1>A Complete Developer Toolset</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, totam!</p>
@@ -46,33 +46,31 @@ export default {
                     <div>Current Version v2.6.0</div>
 
                 </div>
-                <div class="header_br">
+                <div class="jumbo_right">
                     <img src="/img/about.png" alt="">
                 </div>
             </div>
         </div>
 
-        <div class="bg-white">
-            <svg width="100%" height="50%" id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg"
-                class="transition duration-300 ease-in-out delay-150">
-                <path
-                    d="M 0,400 C 0,400 0,200 0,200 C 175.06666666666666,170.26666666666665 350.1333333333333,140.53333333333333 492,128 C 633.8666666666667,115.46666666666667 742.5333333333333,120.13333333333333 895,135 C 1047.4666666666667,149.86666666666667 1243.7333333333333,174.93333333333334 1440,200 C 1440,200 1440,400 1440,400 Z"
-                    stroke="none" stroke-width="0" fill="#f8f9fc" fill-opacity="1"
-                    class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)">
-                </path>
-            </svg>
-            <div class="brand">
-                <img src="/img/amazon.svg" alt="">
-                <img src="/img/shopify.svg" alt="">
-                <img src="/img/spotify.svg" alt="">
-                <img src="/img/google.svg" alt="">
-                <img src="/img/lenovo.svg" alt="">
-                <img src="/img/paypal.svg" alt="">
-            </div>
+        
+    </div>
+    <div class="bg-white">
+        <div>
 
+            <img src="../../public/img/waves.png" alt="">
         </div>
 
-</div></template>
+        <div class="brand">
+            <img src="/img/amazon.svg" alt="">
+            <img src="/img/shopify.svg" alt="">
+            <img src="/img/spotify.svg" alt="">
+            <img src="/img/google.svg" alt="">
+            <img src="/img/lenovo.svg" alt="">
+            <img src="/img/paypal.svg" alt="">
+        </div>
+
+    </div>
+</template>
 
 
 <style ></style>
--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import {  faMagnifyingGlass, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import {  faChevronDown, faMagnifyingGlass, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faUserSecret,faStackOverflow,faUser,faGithub,faMagnifyingGlass)
+library.add(faUserSecret,faStackOverflow,faUser,faGithub,faMagnifyingGlass,faChevronDown)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
